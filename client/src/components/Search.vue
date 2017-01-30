4c118d8e7cf6da90d8f67b318560e0f29e69096f
Samuel Martineau <martineau.samuel.1990@gmail.com>
2017-01-31 00:54:14 +0100

    debounce request

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,19 +1,33 @@
 <template>
 <div class="search">
   <input v-model="query" placeholder="edit me">
-  <p v-if="query">Number of results: {{ results }}</p>
+  <p v-if="query">Number of results: {{ results.total }}</p>
+  <ul>
+    <li v-for="hit in results.hits">
+      <p v-for="sentence in hit._source.texteJuriJudi.texte.blocTextuel.contenu">
+        {{ sentence }}
+      </p>
+    </li>
+  </ul>
 </div>
 </template>
 
 <script>
+import debounce from 'lodash/debounce'
+
 export default {
   name: 'search',
   data: () => ({
     query: '',
     results: 0
   }),
-  watch: {
-    'query': function (val, oldVal) {
+  computed: {
+    throttleUpdates () {
+      return debounce(this.updateFilters, 300)
+    }
+  },
+  methods: {
+    updateFilters (val, oldVal) {
       this.$http.post('http://192.168.99.100:9200/_search', {
         query: {
           match: {
@@ -21,8 +35,13 @@ export default {
           }
         }
       }).then((response) => {
-        this.results = response.body.hits.total
+        this.results = response.body.hits
       })
+    }
+  },
+  watch: {
+    'query': function (val, oldVal) {
+      this.throttleUpdates(val, oldVal)
     }
   }
 }
